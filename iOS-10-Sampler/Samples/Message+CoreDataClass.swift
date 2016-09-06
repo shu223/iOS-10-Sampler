c4495d98c2e22d20bb8e4c5fabff84a7ca7b5f66
Noritaka Kamiya <noritakakamiya@gmail.com>
2016-09-06 22:50:22 +0900

    Added Core Data example

---
+++ iOS-10-Sampler/Samples/Message+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Message+CoreDataClass.swift
+//  iOS-10-Sampler
+//
+//  Created by Noritaka Kamiya on 2016/08/31.
+//  Copyright © 2016 Noritaka Kamiya. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Message)
+public class Message: NSManagedObject {
+
+}
