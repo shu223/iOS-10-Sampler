9febaea67c10026252cc59d7a42255e558d669b9
shu223 <shuichi0526@gmail.com>
2016-09-13 02:42:13 +0900

    Basic implementation of Looper sample

--- iOS-10-Sampler/Samples/LooperViewController.swift
+++ iOS-10-Sampler/Samples/LooperViewController.swift
@@ -10,26 +10,28 @@ import UIKit
 
 class LooperViewController: UIViewController {
 
+    private var looper: PlayerLooper!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        guard let videoPath = Bundle.main.path(forResource: "superquest", ofType: "mp4") else {fatalError()}
+        let videoUrl = URL(fileURLWithPath: videoPath)
+        looper = PlayerLooper(videoURL: videoUrl, loopCount: -1)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        looper.start(in: view.layer)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        looper.stop()
     }
-    */
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
 }
